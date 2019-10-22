4b38d53808c3b46ee9edeaf0726f75c56faf05c0
Aleksei Remnev <ran1024@yandex.ru>
2019-10-22 16:46:31 +1000

    Complit test environment

---
+++ conftest.py
@@ -0,0 +1,34 @@
+# Обработка командной строки.
+# Указываем тип браузера.
+# Указываем язык браузера с помощью WebDriver.
+import pytest
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default='chrome',
+                    help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default='en', help="Choose language")
+    
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    user_language = request.config.getoption("language")
+    browser = None
+    if browser_name == "chrome":
+        print("\nStart chome browser for test...")
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        print("\nStart firefox browser for test...")
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference('intl.accept_languages', user_language)
+        browser = webdriver.Firefox(firefox_profile=fp)
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    yield browser
+    print("\nClose browser...")
+    browser.quit()
