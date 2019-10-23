46950085f5e1e0bcbb8420a8a1f81ff02341906e
Aleksei Remnev <ran1024@yandex.ru>
2019-10-23 19:18:20 +1000

    Инициализируем LoginPage в теле теста.

--- test_main_page.py
+++ test_main_page.py
@@ -15,10 +15,12 @@ def test_guest_can_do_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
     
-def test_guest_should_be_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/accounts/login/"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_login_page()
+#def test_guest_should_be_login_page(browser):
+#    link = "http://selenium1py.pythonanywhere.com/accounts/login/"
+#    page = LoginPage(browser, link)
+#    page.open()
+#    page.should_be_login_page()
     
