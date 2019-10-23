bb7d8ca558c88275f879c0c163526aab4d26e902
Aleksei Remnev <ran1024@yandex.ru>
2019-10-23 17:59:53 +1000

    Проверка форм на странице логина

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,7 @@
 # pytest -v --tb=line --language=en test_main_page.py
 #
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 def test_guest_should_see_login_link(browser):
@@ -14,4 +15,10 @@ def test_guest_can_do_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+    
+def test_guest_should_be_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_page()
     
