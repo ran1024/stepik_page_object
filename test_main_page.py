3180993e7441d4dcff21069775bf3215a6f84158
Aleksei Remnev <ran1024@yandex.ru>
2019-10-23 15:31:21 +1000

    Сделан тест перехода на страницу логина

--- test_main_page.py
+++ test_main_page.py
@@ -1,14 +1,11 @@
 # pytest -v --tb=line --language=en test_main_page.py
 #
-link = "http://selenium1py.pythonanywhere.com/"
-
-
-def go_to_login_page(browser):
-    link = browser.find_element_by_css_selector("#login_link")
-    link.click()
+from .pages.main_page import MainPage
 
 
 def test_guest_can_do_to_login_page(browser):
-    browser.get(link)
-    go_to_login_page(browser)
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
     
