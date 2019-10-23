5f04f864b3ffe67ca312c4f390cd9757a86dd508
Aleksei Remnev <ran1024@yandex.ru>
2019-10-23 16:20:03 +1000

    Перехват исключений вынесен в метод is_element_present

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,13 +1,20 @@
-
+from selenium.common.exceptions import NoSuchElementException
 
 class BasePage():
     """
     Базовая страница, от которой будут унаследованы все остальные классы.
     """
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
     
     def open(self):
         self.browser.get(self.url)
-        
+    
+    def is_element_present(self, locator, data):
+        try:
+            self.browser.find_element(locator, data)
+        except NoSuchElementException:
+            return False
+        return True
